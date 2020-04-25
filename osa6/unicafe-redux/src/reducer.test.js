96dd840cd9941c082b07af42d3129dbe10bb3959
plassi <puurunen.lassi@gmail.com>
2020-04-25 22:46:46 +0300

    unicafe-redux first commit

---
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,91 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('neutral incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  describe('All state values are 1', () => {
+    const initialState = {
+      good: 1,
+      ok: 1,
+      bad: 1
+    }
+
+    test('ZERO resets state values to 0', () => {
+      const action = {
+        type: 'ZERO'
+      }
+      const state = initialState
+  
+      deepFreeze(state)
+      const newState = counterReducer(state, action)
+      expect(newState).toEqual({
+        good: 0,
+        ok: 0,
+        bad: 0
+      })
+    })
+
+
+  })
+
+})
