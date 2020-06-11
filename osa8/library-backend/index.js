cf0a61c7bd7dbe7dad17eaab2c55469a709973b9
plassi <puurunen.lassi@gmail.com>
2020-06-11 21:26:37 +0300

    8.4 tehty

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -100,7 +100,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -109,7 +109,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      const byAuthor = (book) =>
+        args.author === book.author ? book : !book
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors
   },
   Author: {
