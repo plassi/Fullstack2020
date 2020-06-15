7a2210503d73512e46dcbfc85eca7397a1f8d2d9
plassi <puurunen.lassi@gmail.com>
2020-06-15 14:53:47 +0300

    8.10 tehty

--- osa8/library-frontend/src/components/NewBook.js
+++ osa8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,14 +10,20 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }]
+  })
+
   if (!props.show) {
     return null
   }
 
   const submit = async (event) => {
     event.preventDefault()
-    
-    console.log('add book...')
+
+    const publishedInt = Number(published)
+
+    addBook({ variables: { title, author, published: publishedInt, genres } })
 
     setTitle('')
     setPublished('')
