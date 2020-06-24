f6a2ed3f967d5f311b905bf792a846bc97ab3d5d
plassi <puurunen.lassi@gmail.com>
2020-06-24 20:51:34 +0300

    9.4 tehty

--- osa9/ts-exercise/bmiCalculator.ts
+++ osa9/ts-exercise/bmiCalculator.ts
@@ -10,6 +10,7 @@ const calculateBmi = (height: number, weight: number) => {
   if (bmi >= 18.5 && bmi < 25) return 'Normal (healthy weight)';
   if (bmi >= 25 && bmi < 30) return 'Overweight';
   if (bmi >= 30) return 'Obese';
+  return null
 }
 
 const parseBmiArguments = (args: Array<string>): Values => {
